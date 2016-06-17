59920b8b74b8d2e1bb5925a4209640c2955145a0
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-06-17 17:34:07 -0300

    ADD a Class HolaMundo Tipos de Datos
    	modified:   src/trainer_java/HolaMundo.java

--- src/trainer_java/HolaMundo.java
+++ src/trainer_java/HolaMundo.java
@@ -5,6 +5,26 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		// Tipos de Datos Enteros
+		byte  i = 100; 			// 1 bytes 128
+		short j = 30000; 		// 2 bytes 32768
+		int   k = 2000000000; 	// 4 bytes 2*10^9
+		long  l = 1223231232; 	// 8 bytes 9*10^18
+		
+		// Tipos de Datos FLOAT y DOUBLE
+		float m = 2.40f;
+		double n = 2.40;
+		
+		// Tipos Char (comillas simples)
+		char genero = 'F';
+		
+		// Boolean
+		boolean status = true;
+		
+		// Cast de variables
+		double d = 4.59;
+		int d2 = (int) d;		 
 	}
 
 }
