7c2a4a6043e2b721d43a0394932f90973f322b97
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-06-22 17:54:58 -0300

    MODIFY class Array
    Se Agregan array Bidimensional y Tridimensional
    
    	modified:   src/trainer_java/Arreglo.java

--- src/trainer_java/Arreglo.java
+++ src/trainer_java/Arreglo.java
@@ -4,13 +4,19 @@ public class Arreglo {
 
 	public static void main(String[] args) {
 		// Declarando el Array
-		int[] MiArray = new int[5];
+		int[] 		MiArray = new int[5];
+		int[][] 	MiArrayBi = new int[5][5];
+		int[][][] 	MiArrayTri = new int[3][3][3];
 		
 		// Asignando Valor (SET)
 		MiArray[0] = 666;
+		MiArrayBi[0][0] = 34;
+		MiArrayTri[1][1][1] = 78;
 		
 		// Accediendo al valor (GET)
 		System.out.println(MiArray[0]);
+		System.out.println(MiArrayBi[0][0]);
+		System.out.println(MiArrayTri[1][1][1]);
 
 	}
 
