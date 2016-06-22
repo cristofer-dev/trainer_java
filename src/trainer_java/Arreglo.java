a05f0e2b972b040884c6ab04732228ec5c5bc162
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-06-22 17:46:02 -0300

    ADD class Arreglo
    - Se Declara array unidimensional
    - Se Asigna valor (set)
    - Se lee valor (get)
    
    	new file:   src/trainer_java/Arreglo.java

---
+++ src/trainer_java/Arreglo.java
@@ -0,0 +1,17 @@
+package trainer_java;
+
+public class Arreglo {
+
+	public static void main(String[] args) {
+		// Declarando el Array
+		int[] MiArray = new int[5];
+		
+		// Asignando Valor (SET)
+		MiArray[0] = 666;
+		
+		// Accediendo al valor (GET)
+		System.out.println(MiArray[0]);
+
+	}
+
+}
