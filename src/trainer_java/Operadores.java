20fc2ab48de0a4dd67b9addaa4d11f5305761b6a
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-06-23 18:06:39 -0300

    ADD class Operadores
    Ejemplo de operadores matematicos basicos
    
    	new file:   src/trainer_java/Operadores.java

---
+++ src/trainer_java/Operadores.java
@@ -0,0 +1,33 @@
+package trainer_java;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// Operadores
+		int a = 2;
+		int b = 5;
+		int c = 0;
+		
+		// SUMA
+		c = a + b;
+		System.out.println("La Suma es : " + c);
+		
+		// RESTA
+		c = a - b ;
+		System.out.println("La Resta es :" + c);
+		
+		// MULTIPLICACION
+		c = a * b ;
+		System.out.println("La Multiplicacion es :" + c);
+		
+		// DIVISION
+		c = b / a ;
+		System.out.println("La Divición es :" + c);
+		
+		// MODULO
+		c = b % a;
+		System.out.println("El Modulo es :" + c);
+
+	}
+
+}
