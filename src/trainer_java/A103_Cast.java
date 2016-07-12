967251c179cc205f7ecc356464944c0aa2296311
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-07-12 16:53:08 -0300

    ADD A103_Cast
    	new file:   src/trainer_java/A103_Cast.java

---
+++ src/trainer_java/A103_Cast.java
@@ -0,0 +1,14 @@
+package trainer_java;
+
+public class A103_Cast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Cast de variables
+		double d = 4.59;
+		int d2 = (int) d;	
+		
+		System.out.println(d2);
+	}
+
+}
