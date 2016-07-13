0d7719dba278e92229788fae8bc21c9a3bafef5a
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-07-13 17:08:15 -0300

    ADD A104_Array
    	new file:   src/trainer_java/A104_Array.java

---
+++ src/trainer_java/A104_Array.java
@@ -0,0 +1,23 @@
+package trainer_java;
+
+public class A104_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declarando el Array
+		int[] 		MiArray = new int[5];
+		int[][] 	MiArrayBi = new int[5][5];
+		int[][][] 	MiArrayTri = new int[3][3][3];
+		
+		// Asignando Valor (SET)
+		MiArray[0] = 666;
+		MiArrayBi[0][0] = 34;
+		MiArrayTri[1][1][1] = 78;
+		
+		// Accediendo al valor (GET)
+		System.out.println(MiArray[0]);
+		System.out.println(MiArrayBi[0][0]);
+		System.out.println(MiArrayTri[1][1][1]);
+	}
+
+}
