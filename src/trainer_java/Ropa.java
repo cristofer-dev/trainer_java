e420c7aed4a976ef2d53f3bb8860e3ac7328ec84
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-07-20 01:02:05 -0300

    ADD Class  Ropa
    Refactoring de clases Playera, Calcetin y Jeans y se
    crea la clase PADRE "Ropa"
    
    	new file:   Ropa.java

---
+++ src/trainer_java/Ropa.java
@@ -0,0 +1,42 @@
+package trainer_java;
+
+public class Ropa {
+	private String id;
+	private double precio;
+	private String talla;
+	private String color;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getTalla() {
+		return talla;
+	}
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void mostrarDatos(String nombreClase){
+		System.out.println(nombreClase);
+		System.out.println("ID : " + id);
+		System.out.println("Precio : " + precio);
+		System.out.println("Talla : " + talla);
+		System.out.println("Color : " + color);		
+	}
+	
+	
+}
